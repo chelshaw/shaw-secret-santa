a5dfd997a116cd51754dc0a04299fe0cbe6b9311
Chelsea Shaw <chelshaw.dev@gmail.com>
2023-11-16 20:47:19 -0600

    Use edge function for auth/callback

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,5 +1,6 @@
 // src/routes/auth/callback/+server.ts
 import { redirect } from '@sveltejs/kit';
+import type { Config } from '@sveltejs/adapter-vercel';
 
 export const GET = async ({ url, locals: { supabase } }) => {
 	const code = url.searchParams.get('code');
@@ -9,4 +10,8 @@ export const GET = async ({ url, locals: { supabase } }) => {
 	}
 
 	throw redirect(303, '/account');
+};
+
+export const config: Config = {
+	runtime: 'edge'
 };
