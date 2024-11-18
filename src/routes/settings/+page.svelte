da660e09dd4e083f238553a0ebfb92938c4cfd6b
Chelsea Shaw <chelshaw.dev@gmail.com>
2024-11-18 12:56:49 -0600

    Fix do not match behavior on settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import CandyCaneBox from '../../components/candy-cane-box.svelte';
 	import ConfirmEmail from '../../components/confirm-email.svelte';
-	import Error from '../../components/error.svelte';
-	import ProfileSearch from '../../components/profile-search.svelte';
+	import DoNotMatch from '../../components/do-not-match.svelte';
 	import Tabs from '../../components/tabs.svelte';
 	interface Profile {
 		name: string;
 		user_id: string;
 	}
 	export let data;
-	let blockError = '';
-	let matches: Profile[] = [];
 
 	$: profilesById = data.profiles
 		? data.profiles.reduce((obj: Record<string, Profile>, curr: Profile) => {
@@ -21,14 +17,7 @@
 		  }, {})
 		: {};
 
-	function filterProfiles(profiles: Profile[], blocklist: string[], self: string, match: string) {
-		const blocked = [...blocklist, match, self];
-		return profiles.filter((p) => !blocked.includes(p.user_id));
-	}
-	$: profilesLeft = filterProfiles(data.profiles, data.blockList, data.self, data.match);
-	function onMatch(m: Profile[]) {
-		matches = m;
-	}
+	$: blockList = data.blockList.map((b) => profilesById[b]);
 </script>
 
 <Tabs current="settings" />
@@ -40,43 +29,18 @@
 		Add names of participants who you do not want to match with this year (eg. your spouse). You
 		will not be matched with the person you matched with last year, if you participated.
 	</p>
-	<ProfileSearch
-		profiles={profilesLeft}
-		{onMatch}
-		label="Block match with"
-		searchPlaceholder="Search by name"
-		emptyMessage="User not found"
+	<DoNotMatch
+		profiles={data.profiles}
+		self={data.self}
+		blocklist={data.blockList}
+		prevMatch={data.match}
 	/>
-	{#each matches as match}
-		<form
-			class="match"
-			method="POST"
-			action="/settings?/block"
-			use:enhance={() => {
-				blockError = '';
-				return async ({ result, update }) => {
-					if (result.type === 'failure') {
-						console.log('failure: ', result.data?.message);
-					} else {
-						update();
-					}
-				};
-			}}
-		>
-			<input class="readonly" name="name" value={match.name} readonly />
-			<input name="userId" value={match.user_id} type="hidden" />
-			<input name="blocklist" value={data.blockList} type="hidden" />
-			<input type="submit" class="btn btn--match" value="Do not match" />
-		</form>
-	{/each}
-	{#if blockError}
-		<Error debug={blockError}>An error occurred. Please try again later.</Error>
-	{/if}
+
 	<h3>Blocklist</h3>
-	{#each data.blockList as b}
+	{#each blockList as b}
 		<form class="match" method="POST" action="/settings?/unblock">
-			<input class="readonly" name="name" value={profilesById[b].name} readonly />
-			<input name="userId" value={profilesById[b].user_id} type="hidden" />
+			<input class="readonly" name="name" value={b.name} readonly />
+			<input name="userId" value={b.user_id} type="hidden" />
 			<input name="blocklist" value={data.blockList} type="hidden" />
 			<input type="submit" class="btn btn--red btn--match" value="Unblock" />
 		</form>
